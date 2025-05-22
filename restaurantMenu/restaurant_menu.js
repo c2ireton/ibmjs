51e0f875341ae9303d7a8936a93672d327e76d85
Simon <c2ireton@gmail.com>
2025-05-22 19:06:25 -0400

    starting menu

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,16 @@
+const breakfestMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfestMenuHTML = breakfestMenu.map((Item, index) => `<p>Item ${index + 1}: ${Item}</p>`).join('');
+    document.getElementById('breakfestMenuItems').innerHTML = breakfestMenuHTML;
+
+  let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
